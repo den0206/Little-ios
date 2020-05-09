ccca7ae040ea44f855b76f4157d1158f58e77d84
den0206 <mac.pop@mineo.jp>
2020-05-09 22:53:45 +0900

    Initial Commit

---
+++ Little-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Little-ios
+//
+//  Created by 酒井ゆうき on 2020/05/09.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
